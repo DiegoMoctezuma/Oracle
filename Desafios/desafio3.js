ab884d800625f82f8940ab9206a6e243c023dfd7
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-01-23 21:31:08 -0600

    Desafio 3

---
+++ Desafios/desafio3.js
@@ -0,0 +1,37 @@
+//3.1
+let i = 1;
+
+console.log("********D1********");
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+//3.2
+let j = 10;
+
+console.log("********D2********");
+while(j >= 0){
+    console.log(j);
+    j--;
+}
+
+//3.3
+let k = 1;
+let numeroUsuarioK = prompt("Ingrese un número para la cuenta progresiva:");
+
+console.log("********D3********");
+while(k <= numeroUsuarioK){
+    console.log(k);
+    k++;
+}
+
+//3.4
+let l = 1;
+let numeroUsuarioL = prompt("Ingrese un número para la cuenta regresiva:");
+
+console.log("********D4********");
+while(numeroUsuarioL >= l){
+    console.log(numeroUsuarioL);
+    numeroUsuarioL--;
+}
