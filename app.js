002017d2de15063c20b7a8614a9ce460523c774d
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-01-23 18:17:42 -0600

    Numero secreto

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hola Mundo!");
+/* Varibles */ 
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Ingrese un número");
+console.log(numeroUsuario);
+
+if (numeroSecreto == numeroUsuario) {
+    alert("Adivinaste el número secreto!");
+}
