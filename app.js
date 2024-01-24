11dfcae834ab2d28d48be67bc7874aec59495d8f
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-01-23 21:20:13 -0600

    Bucle y contador del juego

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 /* Varibles */ 
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Ingrese un número entre 1 y 10");
-console.log(numeroUsuario);
+let numeroSecreto = 5;
+let numeroUsuario = 0;
+let intentos = 1;
 
-if (numeroSecreto == numeroUsuario) {
-    alert(`Adivinaste, el número secreto es: ${numeroSecreto}`);
-}else {
-    alert('Lo siento, no adivinaste el número');
-}
+/* Bucle del juego */
+do {
+    numeroUsuario = prompt("Ingrese un número entre 1 y 10");
+    console.log(numeroUsuario);
+    
+    if (numeroSecreto == numeroUsuario) {
+        alert(`Adivinaste, el número secreto es: ${numeroSecreto}`);
+        alert(`Número de intentos: ${intentos}`);
+    }else {
+        if (numeroSecreto < numeroUsuario) {
+            alert('El número secreto es menor');
+        }else {
+            alert('El número secreto es mayor');
+        }
+        intentos++;
+        //alert('Lo siento, no adivinaste el número');
+    }
+} while (numeroSecreto != numeroUsuario);
