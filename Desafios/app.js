b8dd555d8ddeb655d5d687b71f8539b605e7f7d2
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-01-23 18:28:44 -0600

    Primer desafio

---
+++ Desafios/app.js
@@ -0,0 +1,17 @@
+/*Variables*/
+let nombre = "Luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+let mensajeDeError = "¡Error! Completa todos los campos";
+
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
+alert("¡Error! Completa todos los campos");
+alert(mensajeDeError);
+
+nombre = prompt("Ingrese su nombre");
+edad = prompt("Ingrese su edad");
+
+if (edad >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
