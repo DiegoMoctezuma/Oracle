0f9cfe5afffb7d83cfc7483e64d98dc3a905d6a4
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-01-23 19:20:02 -0600

    Texto con variables

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 /* Varibles */ 
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Ingrese un número");
+let numeroUsuario = prompt("Ingrese un número entre 1 y 10");
 console.log(numeroUsuario);
 
 if (numeroSecreto == numeroUsuario) {
-    alert("Adivinaste el número secreto!");
+    alert(`Adivinaste, el número secreto es: ${numeroSecreto}`);
+}else {
+    alert('Lo siento, no adivinaste el número');
 }
