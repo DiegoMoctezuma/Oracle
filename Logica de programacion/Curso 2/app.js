621b21695a568e10d430e0cad55187cedb34442a
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-01-25 15:25:49 -0600

    DOM - Document Object Model

---
+++ Logica de programacion/Curso 2/app.js
@@ -0,0 +1,11 @@
+/*DOM (Document Object Model)*/ 
+let titulo =  document.querySelector("h1"); //Objeto
+titulo.innerHTML = "Juego del número secreto";
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Indica un número del 1 al 10";
+
+//Funciones
+function intentoUsuario() {
+    alert("Has hecho click");
+}
