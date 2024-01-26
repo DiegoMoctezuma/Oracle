4dfb4927146e000145cb8aef0c2fda11fd29b0c5
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-01-25 21:05:55 -0600

    Ciclo del juego

--- Logica de programacion/Curso 2/app.js
+++ Logica de programacion/Curso 2/app.js
@@ -1,3 +1,7 @@
+//Variables
+let numSecreto = 0;
+let intentos = 0;   
+
 //Funciones
 function asignarTextoElemento (elemento, texto) {
     let elementoHTML =  document.querySelector(elemento); //Objeto
@@ -7,7 +11,40 @@ function asignarTextoElemento (elemento, texto) {
 
 function verificarIntento() {
     let numeroUsuario = parseInt(document.getElementById("numeroUsuario").value);
-    console.log(typeof numeroUsuario);
+    if(numeroUsuario === numSecreto){
+        //Usuario adivino
+        asignarTextoElemento("p", `Adivinaste el número secreto en ${intentos} ${intentos === 1 ? "intento!" : "intentos!"}`);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    } else {
+        //Usuario no adivino
+        if(numeroUsuario > numSecreto){
+            asignarTextoElemento("p", "El número secreto es menor");
+        } else {
+            asignarTextoElemento("p", "El número secreto es mayor");
+        }
+        intentos++;
+        limpiarInput();
+    }
+    return;
+}
+
+function limpiarInput(){
+    document.querySelector("#numeroUsuario").value = "";
+    return;
+}
+
+function condicionesIniciales(){
+    /*DOM (Document Object Model)*/ 
+    asignarTextoElemento("h1", "Juego del numero secreto");
+    asignarTextoElemento("p", "Indica un número del 1 al 10");
+    numSecreto = numAleat();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    limpiarInput();
+    condicionesIniciales();
+    document.getElementById("reiniciar").setAttribute("disabled",true); //Colocar atributo con valor true
     return;
 }
 
@@ -15,9 +52,4 @@ function numAleat() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
-let numSecreto = numAleat();
-console.log(numSecreto);
-
-/*DOM (Document Object Model)*/ 
-asignarTextoElemento("h1", "Juego del numero secreto");
-asignarTextoElemento("p", "Indica un número del 1 al 10");
+condicionesIniciales();
