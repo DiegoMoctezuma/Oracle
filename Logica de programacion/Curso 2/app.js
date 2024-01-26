c0d4ff94ce5ffba80b1cb2f2ab6e135545932f7f
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-01-25 18:39:43 -0600

    Adicion de funciones para verificar el intento del usuario y generar un numero aleatorio

--- Logica de programacion/Curso 2/app.js
+++ Logica de programacion/Curso 2/app.js
@@ -1,11 +1,23 @@
-/*DOM (Document Object Model)*/ 
-let titulo =  document.querySelector("h1"); //Objeto
-titulo.innerHTML = "Juego del número secreto";
-
-let parrafo = document.querySelector("p");
-parrafo.innerHTML = "Indica un número del 1 al 10";
-
 //Funciones
-function intentoUsuario() {
-    alert("Has hecho click");
+function asignarTextoElemento (elemento, texto) {
+    let elementoHTML =  document.querySelector(elemento); //Objeto
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+function verificarIntento() {
+    let numeroUsuario = parseInt(document.getElementById("numeroUsuario").value);
+    console.log(typeof numeroUsuario);
+    return;
+}
+
+function numAleat() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+let numSecreto = numAleat();
+console.log(numSecreto);
+
+/*DOM (Document Object Model)*/ 
+asignarTextoElemento("h1", "Juego del numero secreto");
+asignarTextoElemento("p", "Indica un número del 1 al 10");
